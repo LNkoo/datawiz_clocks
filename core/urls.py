277b929d314cc5e0211420802cda78652e4e0ba7
Lena <zozyliaolena@gmail.com>
2018-11-05 21:19:20 +0200

    create urls for app

---
+++ core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from core.views import index_core
+
+
+urlpatterns = [
+    path('', index_core),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index_core(request):
+    return HttpResponse('<h1>Hello world</h1>')
