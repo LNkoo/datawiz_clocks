19adc3360c9fbc3390ccf563f4ebfafcb06abfc0
Julia <juliagamanzhy@mail.ua>
2018-11-16 10:46:00 +0200

    ability to undo or redo in department

--- core/static/core/js/departments.js
+++ core/static/core/js/departments.js
@@ -1,14 +1,65 @@
 $(document).ready(function () {
-    var departmentModel = {departments: ko.observableArray([])};
-    ko.applyBindings(departmentModel, $('.departments')[0]);
+    var storeModel = {
+        departments: ko.observableArray([]),
+        showDepartments: ko.observable(true),
+        groupOfProducts: ko.observableArray([]),
+        showGroupOfProducts: ko.observable(false),
+        products: ko.observableArray([]),
+        showProducts: ko.observable(false),
+        uploadItems: uploadItems,
+        undo: undo,
+        redo: redo,
+    };
+    ko.applyBindings(storeModel, $('.store')[0]);
 
     $.ajax({
         url: "/api/departments/",
         context: document.body
     }).done(function (data) {
-        departmentModel.departments(data);
+        storeModel.departments(data);
     });
-});
 
+    function uploadItems(item) {
+        if (item.groups_url) {
+            $.ajax({
+                url: item.groups_url,
+                context: document.body
+            }).done(function (data) {
+                storeModel.groupOfProducts(data);
+                storeModel.showDepartments(false);
+                storeModel.showGroupOfProducts(true);
+            })
+        } else if (item.products_url) {
+            $.ajax({
+                url: item.products_url,
+                context: document.body
+            }).done(function (data) {
+                storeModel.products(data.results);
+                storeModel.showGroupOfProducts(false);
+                storeModel.showProducts(true);
+            })
+        }
+    }
+
+    function undo() {
+        if (storeModel.showGroupOfProducts()) {
+            storeModel.showGroupOfProducts(false);
+            storeModel.showDepartments(true);
+        } else if (storeModel.showProducts) {
+            storeModel.showProducts(false);
+            storeModel.showGroupOfProducts(true);
+        }
+    }
+
+    function redo() {
+        if (storeModel.showGroupOfProducts() && storeModel.products().length>0) {
+            storeModel.showGroupOfProducts(false);
+            storeModel.showProducts(true);
+        } else if (storeModel.showDepartments() && storeModel.groupOfProducts().length>0) {
+            storeModel.showDepartments(false);
+            storeModel.showGroupOfProducts(true);
+        }
+    }
+});
 
 
